59e7329eff1c3151f811e95e347676afa0bb7818
goochang <cck742@naver.com>
2021-11-18 17:35:12 +0900

    semi finish

---
+++ src/routes/Auth.js
@@ -0,0 +1,33 @@
+import { authService, firebaseInstance } from "fbase";
+import AuthForm from "components/AuthForm";
+
+function Auth() {
+
+    const onSocialClick = async (event) => {
+        const { target : {name}, } = event;
+        let provider;
+        if(name==="google"){
+            provider = new firebaseInstance.auth.GoogleAuthProvider();
+        } else if( name === "github"){
+            provider = new firebaseInstance.auth.GithubAuthProvider();
+        }
+        await authService.signInWithPopup(provider);
+    }
+  return (
+    <div>
+        <AuthForm />
+
+        <div>
+            <button onClick={onSocialClick} name="google">
+                Continue with Google
+                </button>
+            <button onClick={onSocialClick} name="github">
+                Continue with Github
+            </button>
+        </div>
+    </div>
+
+  );
+}
+
+export default Auth;
