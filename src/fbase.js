53f61374db7424f6498ea33800f2dd9695d498bf
goochang <cck742@naver.com>
2022-01-13 17:22:00 +0900

    구글 로그인 관련 firebase 호스팅 설정

--- src/fbase.js
+++ src/fbase.js
@@ -1,4 +1,5 @@
 import firebase from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 import "firebase/storage";
@@ -15,11 +16,13 @@ const firebaseConfig = {
     storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
     messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
     appId: process.env.REACT_APP_APP_ID,
-    databaseURL: process.env.REACT_APP_DATABASE_URL
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    measurementId: process.env.REACT_APP_MEASUREMENTID
 };
 
-firebase.initializeApp(firebaseConfig);
 
+const app = firebase.initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
 export const firebaseInstance = firebase;
 export const authService = firebase.auth();
 export const dbService = firebase.firestore();
